a331d349bde3ea30a050422bcffa6089b84e1c61
xanndachet <linhhuyen037@gmail.com>
2023-10-27 20:20:57 +0700

    push code

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.fzwyn5c.mongodb.net',
+    DB_USER: 'Vansang',
+    DB_PASS: 'vansang',
+    DB_DATABASE: 'Shoping_pet',
+    EMAIL_USER: '', // Microsoft mail service
+    EMAIL_PASS: '',
+    JWT_SECRET: 'sang',
+    JWT_EXPIRES: '10000000000000000', // in milliseconds
+};
+module.exports = MyConstants;
